6adced96b6cdbba2e188fcbc0d4c4a88012f4893
xchapeu <xchapeu@gmail.com>
2023-01-01 15:34:20 -0300

    Add 'GAME OVER' funcionality

--- js/validation.js
+++ js/validation.js
@@ -2,10 +2,21 @@ function verifyValidyTry(chute) {
     const num = +chute;
     const chuteElement = document.getElementById('chute');
 
-    if(dataFormatInvalid(num)) {
-        chuteElement.innerHTML += '<div>O valor precisa ser um número</div>';
-        return;
+    if (dataFormatInvalid(num)) {
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                    <h1>Game Over!!!</h1>
+                    <h3>Pressione o botão para jogar novamente</h3>
+                    <button id="playAgain" class="playAgain" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+            chuteElement.innerHTML += '<div>O valor precisa ser um número</div>';
+        }
     }
+
     
     if(numberOutOfBoundaries(num)) {
         chuteElement.innerHTML += `<div>Número fora do range. O número precisa estar entre ${minNumber} e ${maxNumber}</div>`;
@@ -18,14 +29,18 @@ function verifyValidyTry(chute) {
             <h3>Você acertou o número secreto: ${secretNumber}.</h3>
             <button id="playAgain" class="playAgain">Jogar novamente</button>
         `;  
-        const playAgain = document.getElementById('playAgain');
-        playAgain.addEventListener('click', () => location.reload());
     } else if(majorValue(num)) {
         chuteElement.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`;
     } else if(minorValue(num)) {
         chuteElement.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`;
     }
 }
+
+document.body.addEventListener('click', event => {
+    if(event.target.id == 'playAgain') {
+        window.location.reload();
+    }
+})
 
 function dataFormatInvalid(num) {
     return Number.isNaN(num);
